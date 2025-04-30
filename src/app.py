3da7f0c0819219fd318ce608c00aefcfbc0eeb07
Bob <78376753+Bobike123@users.noreply.github.com>
2025-04-30 11:35:33 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 
+
 from pynq.overlays.base import BaseOverlay
 from pynq.lib.video import *
 base = BaseOverlay("base.bit")
 
 ### Step 2: Initialize Webcam and HDMI Out
-
 # monitor configuration: 640*480 @ 60Hz
 Mode = VideoMode(640,480,24)
 hdmi_out = base.video.hdmi_out
@@ -33,63 +33,65 @@ print("Capture device is open: " + str(videoIn.isOpened()))
 
 # Capture webcam image
 import numpy as np
+import time
+while(True):
+    ret, frame_vga = videoIn.read()
 
-ret, frame_vga = videoIn.read()
+    # Display webcam image via HDMI Out
+    if (ret):      
+        outframe = hdmi_out.newframe()
+        outframe[0:480,0:640,:] = frame_vga[0:480,0:640,:]
+        hdmi_out.writeframe(outframe)
+    else:
+        raise RuntimeError("Failed to read from camera.")
 
-# Display webcam image via HDMI Out
-if (ret):      
-    outframe = hdmi_out.newframe()
+    ### Step 4: Now use matplotlib to show image inside notebook
+
+    # Output webcam image as JPEG
+
+    from matplotlib import pyplot as plt
+    import numpy as np
+    plt.imshow(frame_vga[:,:,[2,1,0]])
+    plt.show()
+
+    ### Step 5: Apply the face detection to the input
+
+    np_frame = frame_vga
+
+    face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('./data/haarcascade_eye.xml')
+
+    gray = cv2.cvtColor(np_frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(np_frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = np_frame[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    ### Step 6: Show results on HDMI output
+
+    # Output OpenCV results via HDMI
     outframe[0:480,0:640,:] = frame_vga[0:480,0:640,:]
+    cv2.imwrite("out.png",outframe)
     hdmi_out.writeframe(outframe)
-else:
-    raise RuntimeError("Failed to read from camera.")
 
-### Step 4: Now use matplotlib to show image inside notebook
+    ### Step 7: Now use matplotlib to show image inside notebook
 
-# Output webcam image as JPEG
 
-from matplotlib import pyplot as plt
-import numpy as np
-plt.imshow(frame_vga[:,:,[2,1,0]])
-plt.show()
+    # Output OpenCV results via matplotlib
 
-### Step 5: Apply the face detection to the input
+    from matplotlib import pyplot as plt
+    import numpy as np
+    plt.imshow(np_frame[:,:,[2,1,0]])
+    plt.show()
+    ### Step 8: Release camera and HDMI
 
-np_frame = frame_vga
-
-face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('data/haarcascade_eye.xml')
-
-gray = cv2.cvtColor(np_frame, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("out.png",gray)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-for (x,y,w,h) in faces:
-    cv2.rectangle(np_frame,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = np_frame[y:y+h, x:x+w]
-
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-### Step 6: Show results on HDMI output
-
-# Output OpenCV results via HDMI
-outframe[0:480,0:640,:] = frame_vga[0:480,0:640,:]
-hdmi_out.writeframe(outframe)
-
-### Step 7: Now use matplotlib to show image inside notebook
-
-# Output OpenCV results via matplotlib
-
-from matplotlib import pyplot as plt
-import numpy as np
-plt.imshow(np_frame[:,:,[2,1,0]])
-plt.show()
-
-### Step 8: Release camera and HDMI
-
+    time.sleep(1)
 videoIn.release()
 hdmi_out.stop()
 del hdmi_out
